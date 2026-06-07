bd3efabebe3dfe97de0ba3ec7c084325dd7c0764
Alexander Ortiz <ortiz.alexander1808@gmail.com>
2026-06-07 11:22:42 -0600

    Agrego Calculadora de Gastos V2 con validación de fondos insuficientes

---
+++ Experimentos/calculadora-gastos-v1.py
@@ -0,0 +1,14 @@
+print("Bienvenido a tu calculadora de gastos. \n")
+Presupuesto_inicial = int(input("¿Cúal es tu presupuesto del dia de hoy? "))
+while Presupuesto_inicial > 0:
+    Compras = input("¿Qué compraste? ")
+    Gastos = int(input("¿Cuánto gastaste en eso? "))
+    Presupuesto_inicial -= Gastos
+    Respuesta = input("¿Quieres registrar otro gasto? ")
+    if Respuesta.lower().strip() == "si" or Respuesta.lower().strip() == "sí":
+        print("Okay, continuamos.")
+    elif Respuesta.lower().strip() == "no".lower().strip():
+        print("Muy bien, tu saldo actual es de", Presupuesto_inicial, "pesos")
+        break
+if Presupuesto_inicial == 0:
+    print("Te haz quedados sin fondos, bro")
---
+++ Experimentos/calculadora-gastos-v2.py
@@ -0,0 +1,18 @@
+print("Bienvenido a tu calculadora de gastos. \n")
+Presupuesto_inicial = int(input("¿Cúal es tu presupuesto del dia de hoy? "))
+while Presupuesto_inicial > 0:
+    Compras = input("¿Qué comprarás? ")
+    Gastos = int(input("¿Cuánto gastarás en eso? "))
+    if Gastos > Presupuesto_inicial:
+        print("¡Fondos insuficientes! No puedes comprar eso")
+    else:
+        Presupuesto_inicial -= Gastos
+        print(f"Gasto registrado. Te quedarán {Presupuesto_inicial} pesos.\n")
+    Respuesta = input("¿Quieres registrar otro gasto? ")
+    if Respuesta.lower().strip() == "si" or Respuesta.lower().strip() == "sí":
+        print("Okay, continuamos. ")
+    elif Respuesta.lower().strip() == "no":
+        print("Muy bien, tu saldo actual es de", Presupuesto_inicial, "pesos")
+        break
+if Presupuesto_inicial == 0:
+    print("Te haz quedados sin fondos, bro")
